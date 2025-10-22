3e32bf292ab63b76932e7cb4721190403bdaab39
Alfonce Morara <alfoncemorara412@gmail.com>
2025-10-22 11:47:23 +0300

    new commit

--- Server/MagicStreamServer/main.go
+++ Server/MagicStreamServer/main.go
@@ -47,8 +47,7 @@ func main() {
 	config := cors.Config{}
 	config.AllowOrigins = origins
 	config.AllowMethods = []string{"GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"}
-	//config.AllowHeaders = []string{"Origin", "Content-Type", "Accept", "Authorization"}
-	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization"}
+	 config.AllowHeaders = []string{"Origin", "Content-Type", "Accept", "Authorization"}
 	config.ExposeHeaders = []string{"Content-Length"}
 	config.AllowCredentials = true
 	config.MaxAge = 12 * time.Hour
